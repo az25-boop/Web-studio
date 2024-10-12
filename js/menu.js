1512495721fbf5a3ead8a95e887a11b69275a186
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-10-12 11:46:38 +0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,20 @@
-
-  
 (() => {
-    const mobileRefs = {
-      menuBtnRef : document.querySelector("[data-menu-btn]"),
-      mobileMenuRef: document.querySelector("[data-menu]"),
-      // modal: document.querySelector("[data-modal]"),
-    };
-  
-    mobileRefs.menuBtnRef.addEventListener("click", toggleMobileMenu);
-     
+  const mobileRefs = {
+    menuBtnRef: document.querySelector("[data-menu-btn]"),
+    mobileMenuRef: document.querySelector("[data-menu]"),
+    // modal: document.querySelector("[data-modal]"),
+  };
+
+  mobileRefs.menuBtnRef.addEventListener("click", toggleMobileMenu);
+
   function toggleMobileMenu() {
     const expanded =
       mobileRefs.menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-      mobileRefs.menuBtnRef.classList.toggle("is-open");
-      mobileRefs.menuBtnRef.setAttribute("aria-expanded", !expanded);
+    mobileRefs.menuBtnRef.classList.toggle("is-open");
+    mobileRefs.menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-      mobileRefs.mobileMenuRef.classList.toggle("is-open");
+    mobileRefs.mobileMenuRef.classList.toggle("is-open");
 
-      document.body.classList.toggle("mobile-menu-open");
-    }
-  })();
+    document.body.classList.toggle("mobile-menu-open");
+  }
+})();
